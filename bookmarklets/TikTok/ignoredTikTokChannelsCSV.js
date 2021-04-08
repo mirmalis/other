e950260e7da74b1656b86a692b81070b6620a034
mirmalis <sarunas1991@gmail.com>
2021-04-08 14:11:23 +0300

    Update ignoredTikTokChannelsCSV.js

--- bookmarklets/TikTok/ignoredTikTokChannelsCSV.js
+++ bookmarklets/TikTok/ignoredTikTokChannelsCSV.js
@@ -1,6 +1,6 @@
 javascript:(
   function(){
-    window.ignoredTikTokChannelsCSV = "_girls.only_60;itzshauni";
+    window.ignoredTikTokChannelsCSV = "_girls.only_60;itzshauni;sex.ed.lt;crafty_emily1;artis_maks;brigapo;salta_arbatele;zania_077;celinaspookyboo;kira_smrn;mr.begedis;kallmekris;kitsy.lt;o.domas;youneszarou;_magzon";
     console.log(window.ignoredTikTokChannelsCSV);
   }()
 )
