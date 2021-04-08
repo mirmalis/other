2eaa9b52b86fe4f36f2c5ce5917a20e7afd25ff1
mirmalis <sarunas1991@gmail.com>
2021-04-08 18:18:45 +0300

    Update showNext.js

--- bookmarklets/TikTok/showNext.js
+++ bookmarklets/TikTok/showNext.js
@@ -1,21 +1,19 @@
 javascript:(
   function(){ 
     function showNext(){
-      if (window.nextElement === undefined || window.nextElement === null){
-        window.nextElement = document.querySelector(".lazyload-wrapper");
-      } else {
-        window.nextElement = window.nextElement.nextElementSibling;
-      }
-      if(window.nextElement !== undefined && window.nextElement !== null){
-        var doSkip = false;
-        if (window.ignoredTikTokChannelsCSV !== undefined && window.ignoredTikTokChannelsCSV !== null){
-          doSkip = window.ignoredTikTokChannelsCSV.split(";").some(item => item === window.nextElement.querySelector(".author-uniqueId").textContent);
+      function getNextElement() {
+        if (window.nextElement === undefined || window.nextElement === null){
+            window.nextElement = document.querySelector(".lazyload-wrapper");
+        } else {
+            window.nextElement = window.nextElement.nextElementSibling;
         }
+        return window.nextElement !== undefined && window.nextElement !== null;
+      }
+      if(getNextElement()){
         var y = window.nextElement.getBoundingClientRect().top + window.pageYOffset + -60;
         window.scrollTo({top: y, behavior: 'smooth'});
-        if(doSkip){
-          return showNext();
-        }
+      } else {
+        console.log('Error: could not find next element.')
       }
     }
     showNext();
