e24521bc85c27bd64f22fd518fd196812b0459b1
mirmalis <sarunas1991@gmail.com>
2021-04-08 12:58:47 +0300

    Fixed spacing

--- bookmarklets/TikTok/showNext.js
+++ bookmarklets/TikTok/showNext.js
@@ -1,23 +1,23 @@
 javascript:(
   function(){ 
     function showNext(){
-        if (window.nextElement === undefined || window.nextElement === null){
-            window.nextElement = document.querySelector(".lazyload-wrapper");
+      if (window.nextElement === undefined || window.nextElement === null){
+        window.nextElement = document.querySelector(".lazyload-wrapper");
+      } else {
+        window.nextElement = window.nextElement.nextElementSibling;
+      }
+      if(window.nextElement !== undefined && window.nextElement !== null){
+        var doSkip = false;
+        if (window.ignoredTikTokChannelsCSV !== undefined && window.ignoredTikTokChannelsCSV !== null){
+          doSkip = window.ignoredTikTokChannelsCSV.split(";").some(item => item === window.nextElement.querySelector(".author-uniqueId").textContent);
+        }
+        if(!doSkip){
+          var y = window.nextElement.getBoundingClientRect().top + window.pageYOffset + -60;
+          window.scrollTo({top: y, behavior: 'smooth'});
         } else {
-            window.nextElement = window.nextElement.nextElementSibling;
+          return showNext()
         }
-        if(window.nextElement !== undefined && window.nextElement !== null){
-            var doSkip = false;
-            if (window.ignoredTikTokChannelsCSV !== undefined && window.ignoredTikTokChannelsCSV !== null){
-                doSkip = window.ignoredTikTokChannelsCSV.split(";").some(item => item === window.nextElement.querySelector(".author-uniqueId").textContent);
-            }
-            if(!doSkip){
-              var y = window.nextElement.getBoundingClientRect().top + window.pageYOffset + -60;
-              window.scrollTo({top: y, behavior: 'smooth'});
-            } else {
-                return showNext()
-            }
-        }
+      }
     }
     showNext();
   }()
