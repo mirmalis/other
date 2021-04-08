4473f1082877e0db6d56543c6899dfa068befb87
mirmalis <sarunas1991@gmail.com>
2021-04-08 12:57:48 +0300

    Create showNext.js

---
+++ bookmarklets/TikTok/showNext.js
@@ -0,0 +1,24 @@
+javascript:(
+  function(){ 
+    function showNext(){
+        if (window.nextElement === undefined || window.nextElement === null){
+            window.nextElement = document.querySelector(".lazyload-wrapper");
+        } else {
+            window.nextElement = window.nextElement.nextElementSibling;
+        }
+        if(window.nextElement !== undefined && window.nextElement !== null){
+            var doSkip = false;
+            if (window.ignoredTikTokChannelsCSV !== undefined && window.ignoredTikTokChannelsCSV !== null){
+                doSkip = window.ignoredTikTokChannelsCSV.split(";").some(item => item === window.nextElement.querySelector(".author-uniqueId").textContent);
+            }
+            if(!doSkip){
+              var y = window.nextElement.getBoundingClientRect().top + window.pageYOffset + -60;
+              window.scrollTo({top: y, behavior: 'smooth'});
+            } else {
+                return showNext()
+            }
+        }
+    }
+    showNext();
+  }()
+)
