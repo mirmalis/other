ce2bd1e8518885c2dbb46c58f476064294061123
mirmalis <sarunas1991@gmail.com>
2021-04-08 14:12:52 +0300

    Update showNext.js

--- bookmarklets/TikTok/showNext.js
+++ bookmarklets/TikTok/showNext.js
@@ -11,10 +11,9 @@ javascript:(
         if (window.ignoredTikTokChannelsCSV !== undefined && window.ignoredTikTokChannelsCSV !== null){
           doSkip = window.ignoredTikTokChannelsCSV.split(";").some(item => item === window.nextElement.querySelector(".author-uniqueId").textContent);
         }
-        if(!doSkip){
-          var y = window.nextElement.getBoundingClientRect().top + window.pageYOffset + -60;
-          window.scrollTo({top: y, behavior: 'smooth'});
-        } else {
+        var y = window.nextElement.getBoundingClientRect().top + window.pageYOffset + -60;
+        window.scrollTo({top: y, behavior: 'smooth'});
+        if(doSkip){
           return showNext();
         }
       }
