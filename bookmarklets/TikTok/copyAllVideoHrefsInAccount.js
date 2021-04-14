74953e050ee028d04ab34ef3b60d6f8aae655876
mirmalis <sarunas1991@gmail.com>
2021-04-14 10:24:15 +0300

    Update copyAllVideoHrefsInAccount.js

--- bookmarklets/TikTok/copyAllVideoHrefsInAccount.js
+++ bookmarklets/TikTok/copyAllVideoHrefsInAccount.js
@@ -1,6 +1,7 @@
 javascript:(
   function(){
     function copyToClipboard(str){
+      console.log(str);
       const el = document.createElement('textarea');
       el.value = str;
       document.body.appendChild(el);
@@ -9,6 +10,6 @@ javascript:(
       document.body.removeChild(el);
     };
     copyToClipboard(Array.from(document.querySelectorAll(".video-feed-item a"))
-    .map(item => item.getAttribute("href")).join(" "));
+    .map(item => item.getAttribute("href")).join("\n"));
   }()
 )
