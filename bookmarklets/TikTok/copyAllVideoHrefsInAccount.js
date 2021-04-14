ffae80e1a19dd5a8848899d373c23be5964da998
mirmalis <sarunas1991@gmail.com>
2021-04-14 10:07:12 +0300

    Create copyAllVideoHrefsInAccount.js

---
+++ bookmarklets/TikTok/copyAllVideoHrefsInAccount.js
@@ -0,0 +1,14 @@
+javascript:(
+  function(){
+    function copyToClipboard(str){
+      const el = document.createElement('textarea');
+      el.value = str;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    };
+    copyToClipboard(Array.from(document.querySelectorAll(".video-feed-item a"))
+    .map(item => item.getAttribute("href")).join(";"));
+  }()
+)
